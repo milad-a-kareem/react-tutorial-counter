92e3dc8808ca3e141ab890c792ed48d92bdac59d
Milad <milad.kareem@gmail.com>
2022-01-16 23:38:02 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import {useState, Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {counter:0}
+  }
+  
+  increaseHandler = ()=>{
+    this.setState({counter:this.state.counter + 1})
+  }
+  decreaseHandler = ()=>{
+    this.setState({counter:this.state.counter - 1})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.increaseHandler}>increase</button>
+        <button onClick={this.decreaseHandler}>decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+// function App() {
+//   const [counterState, setCounterState] = useState(0)
+//   const decreaseHandler = ()=>{
+//     setCounterState(prevState=>{return prevState-=1})
+//   }
+//   const increaseHandler = ()=>{
+//     setCounterState(prevState=>{return prevState+=1})
+//   }
+//   return (
+//     <div className="App">
+//       <h1>{counterState}</h1>
+//       <button onClick={increaseHandler}>increase</button>
+//       <button onClick={decreaseHandler}>decrease</button>
+//     </div>
+//   );
+// }
+
+// class App extends Component {
+//   constructor(){
+//     super()
+//     this.state = {counter:0}
+//   }
+  
+//   increaseHandler = ()=>{
+//     this.setState({counter:this.state.counter + 1})
+//   }
+//   decreaseHandler = ()=>{
+//     this.setState({counter:this.state.counter - 1})
+//   }
+  
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>{this.state.counter}</h1>
+//         <button onClick={this.increaseHandler}>increase</button>
+//         <button onClick={this.decreaseHandler}>decrease</button>
+//       </div>
+//     );
+//   }
+// }
